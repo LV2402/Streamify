ccc3ad33f56b626ee59b92fd657f96ca8b04aee7
LV <vamshi.lagishetty24@gmail.com>
2026-01-03 20:21:53 +0530

    auth backend

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,22 @@
+import {StreamChat} from "stream-chat"
+import "dotenv/config"
+
+const apiKey=process.env.STREAM_API_KEY
+const apiSecret=process.env.STREAM_API_SECRET
+
+if(!apiKey || !apiSecret){
+    console.error("Stream API key and secret are missing");
+}
+
+const streamClient = StreamChat.getInstance(apiKey,apiSecret);
+
+export const upsertStreamUser=async(userData)=>{
+    try{
+        await streamClient.upsertUsers([userData]);
+        return userData;
+    }catch(err){
+        console.error("Error in upserting stream user:",err);
+    }
+}
+
+export const generateStreamToken=(userId)=>{};
